904672204c5fa921dafc27f6293f308332e06c11
Étienne Brouillard <etienne.brouillard@locomotive10.com>
2019-06-18 08:49:14 -0400

    animate led transition

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
+/* eslint-disable no-fallthrough */
 const VAD = require("node-vad");
-const debug = require("debug")("app:app.js");
+const dbg = require("debug");
+const debug = dbg("DEBUG:VAD:app.js");
+const easing = require("./animation/easing");
+const info = dbg("INFO:VAD:app.js");
+const error = dbg("ERROR:VAD:app.js");
 const matrix = require("@matrix-io/matrix-lite");
 const matrixIO = require("matrix-protos").matrix_io;
 const zmq = require("zeromq");
+
+
+const TRANSITIONS = {
+    "SILENCE": 0,
+    "VOICE": 1,
+    "NOISE": 2,
+    "ERROR": -1
+};
 
 const matrixIP = "127.0.0.1";
 const matrixEverloopBasePort = 20021;
 const vad = new VAD(VAD.Mode.NORMAL);
 let matrixDeviceLeds = 0;
+
+const ledAnimationFreq = 1000 / 60;
+let transitionInterval = null;
+let transitionTimeout = null;
+let currentLedState = null;
+let currentTransition = TRANSITIONS.SILENCE;
 
 debug(`starting`);
 debug(`setting up config socket`);
@@ -25,7 +44,6 @@ const ledImage = matrixIO.malos.v1.io.EverloopImage.create();
  * @returns {undefined}
  */
 function show(red = 0, green = 0, blue = 0, white = 0) {
-    debug(`requesting to show leds x ${matrixDeviceLeds} color: ${red} ${green} ${blue} ${white}`);
     for (let idx = 0; idx < matrixDeviceLeds; ++idx) {
         // Set individual LED value
         ledImage.led[idx] = {
@@ -39,8 +57,91 @@ function show(red = 0, green = 0, blue = 0, white = 0) {
         "image": ledImage
     });
     if (matrixDeviceLeds > 0) {
-        configSocket.send(matrixIO.malos.v1.driver.DriverConfig.encode(config).finish()); 
+        configSocket.send(matrixIO.malos.v1.driver.DriverConfig.encode(config).finish());
     }
+}
+
+/**
+ * Gets LED transition target
+ *
+ * @param {TRANSITIONS} target transition value
+ * @returns {object} target transition
+ */
+function getTransitionTarget(target) {
+    switch (target) {
+    case TRANSITIONS.VOICE:
+        return {
+            "red": 0,
+            "green": 100,
+            "blue": 0,
+            "white": 0
+        };
+    case TRANSITIONS.ERROR:
+        return {
+            "red": 100,
+            "green": 0,
+            "blue": 0,
+            "white": 0
+        };
+    case TRANSITIONS.NOISE:
+        return {
+            "red": 0,
+            "green": 100,
+            "blue": 100,
+            "white": 0
+        };
+    default:
+        return {
+            "red": 0,
+            "green": 0,
+            "blue": 0,
+            "white": 0
+        };
+    }
+}
+
+/**
+ * Transition led color
+ * @param {object} target leds color target
+ * @param {number} duration transition duration in ms
+ * 
+ * @returns {undefined}
+ */
+function transitionTo(target = TRANSITIONS.SILENCE, duration = 500) {
+    if (target === currentTransition) {
+        return;
+    }
+    currentTransition = target;
+    if (transitionInterval) {
+        clearInterval(transitionInterval);
+        transitionInterval = null;
+    }
+    if (transitionTimeout) {
+        clearTimeout(transitionTimeout);
+        transitionTimeout = null;
+    }
+    const tween = easing.easeInOutSine;
+    let currentLedTarget = null;
+    let time = 0;
+    currentLedTarget = getTransitionTarget(target, currentLedTarget);
+    const redStart = currentLedState.red;
+    const greenStart = currentLedState.green;
+    const blueStart = currentLedState.blue;
+    const whiteStart = currentLedState.white;
+    transitionInterval = setInterval(() => {
+        currentLedState = {
+            "red": tween(time, redStart, currentLedTarget.red, duration),
+            "green": tween(time, greenStart, currentLedTarget.green, duration),
+            "blue": tween(time, blueStart, currentLedTarget.blue, duration),
+            "white": tween(time, whiteStart, currentLedTarget.white, duration)
+        };
+        show(currentLedState.red, currentLedState.green, currentLedState.blue, currentLedState.white);
+        time += ledAnimationFreq;
+    }, ledAnimationFreq);
+    transitionTimeout = setTimeout(() => {
+        clearInterval(transitionInterval);
+        transitionInterval = null;
+    }, duration);
 }
 
 /**
@@ -54,49 +155,48 @@ function handle(chunk) {
     vad.processAudio(chunk.audioData, 16000).then((res) => {
         switch (res) {
         case VAD.Event.ERROR:
-            show(100, 0, 0, 0); 
+            transitionTo(TRANSITIONS.ERROR);
             break;
         case VAD.Event.NOISE:
-            show(0, 100, 100, 0); 
+            transitionTo(TRANSITIONS.NOISE);
             break;
         case VAD.Event.SILENCE:
-            show(0, 0, 0, 0); 
+            transitionTo(TRANSITIONS.SILENCE);
             break;
         case VAD.Event.VOICE:
-            show(0, 100, 0, 0); 
+            transitionTo(TRANSITIONS.VOICE);
             break;
         default:
             console.log("WTF");
-
         }
     });
 }
 
-debug(`setting up update socket`);
+info(`setting up update socket`);
 const updateSocket = zmq.socket("sub");
 const updatePort = matrixEverloopBasePort + 3;
 updateSocket.connect(`tcp://${matrixIP}:${updatePort}`);
 updateSocket.subscribe("");
-updateSocket.on("message", function(buffer) {
+updateSocket.on("message", function (buffer) {
     const data = matrixIO.malos.v1.io.EverloopImage.decode(buffer);
     debug(`data received from update socket: ${JSON.stringify(data)}`);
     matrixDeviceLeds = data.everloopLength;
 });
 
-debug(`setting up ping socket`);
+info(`setting up ping socket`);
 const pingSocket = zmq.socket("push");
 pingSocket.connect(`tcp://${matrixIP}:${(matrixEverloopBasePort + 1)}`);
 pingSocket.send("");
 
-debug(`setting up error socket`);
+info(`setting up error socket`);
 const errorSocket = zmq.socket("sub");
 errorSocket.connect(`tcp://${matrixIP}:${(matrixEverloopBasePort + 2)}`);
 errorSocket.subscribe("");
-errorSocket.on("message", function(errorMessage) {
-    debug(`Error received: ${errorMessage.toString("utf8")}`);
+errorSocket.on("message", function (errorMessage) {
+    error(`Error received: ${errorMessage.toString("utf8")}`);
 });
 
-debug(`creating microphone instance`);
+info(`creating microphone instance`);
 const micInstance = matrix.alsa.mic({
     "rate": '16000',
     "debug": true,
@@ -108,14 +208,7 @@ const outStream = VAD.createStream({
     "audioFrequency": 16000,
     "debounceTime": 1000
 });
-debug(`getting audio stream`);
+info(`getting audio stream`);
 const micInputStream = micInstance.getAudioStream();
 micInputStream.pipe(outStream).on("data", handle);
 micInstance.start();
-
-setTimeout(() => { 
-    show(100, 100, 100, 0); 
-}, 500);
-setTimeout(() => {
-    show(0, 0, 0, 0);
-}, 1000);
