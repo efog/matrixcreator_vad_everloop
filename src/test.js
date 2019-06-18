51e818a2faffaa97b6b8396fa1a230f17b1463a7
Étienne Brouillard <etienne.brouillard@locomotive10.com>
2019-06-17 21:17:50 -0400

    test write wav

--- src/test.js
+++ src/test.js
@@ -1,29 +1,44 @@
 const VAD = require("node-vad");
+const debug = require("debug")("app:test.js");
 const fs = require("fs");
 const vad = new VAD(VAD.Mode.NORMAL);
 
+// eslint-disable-next-line require-jsdoc
 function handle(chunk) {
-	vad.processAudio(chunk, 16000).then((res) => {
-		console.log(JSON.stringify(res));
-		switch(res) {
-			case VAD.Event.ERROR:
-				console.log("ERROR");
-				break;
-			case VAD.Event.NOISE:
-				console.log("NOISE");
-				break;
-			case VAD.Event.SILENCE:
-				console.log("SILENCE");
-				break;
-			case VAD.Event.VOICE:
-				console.log("VOICE");
-				break;
-			default:
-				console.log("WTF");
+    vad.processAudio(chunk, 16000).then((res) => {
+        console.log(JSON.stringify(res));
+        switch (res) {
+        case VAD.Event.ERROR:
+            console.log("ERROR");
+            break;
+        case VAD.Event.NOISE:
+            console.log("NOISE");
+            break;
+        case VAD.Event.SILENCE:
+            console.log("SILENCE");
+            break;
+        case VAD.Event.VOICE:
+            console.log("VOICE");
+            break;
+        default:
+            console.log("WTF");
 
-		}
-	});
+        }
+    });
 }
+
+const mic = require("mic");
+const micInstance = mic({
+    "rate": 16000,
+    "channels": 1,
+    "debug": true
+});
+
+const micInputStream = micInstance.getAudioStream();
+const outStream = fs.createWriteStream("out.wav");
+micInputStream.pipe(outStream).on("data", (data) => {
+    debug(`received data : ${data}`);
+});
 
 const stream = fs.createReadStream("recording.wav");
 stream.on("data", handle);
